19a705c635235bf413c69f41e4048e4f46a31550
Phạm Việt Hoàng <165120748+phviethoang@users.noreply.github.com>
2026-01-02 17:08:25 +0700

    Update submission.py

--- submission.py
+++ submission.py
@@ -1,6 +1,3 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License.
-
 import os
 import logging
 import torch
@@ -11,9 +8,6 @@ from utility.utils import (setuplogger,  dump_args, check_args_environment)
 from data_handler.preprocess import get_news_feature, infer_news
 from data_handler.TestDataloader import DataLoaderLeader
 from models.speedyrec import MLNR
-
-from time import perf_counter
-from datetime import timedelta
 
 def generate_submission(args):
     setuplogger()
@@ -28,26 +22,10 @@ def generate_submission(args):
     ckpt = torch.load(args.load_ckpt_name)
     model.load_state_dict(ckpt['model_state_dict'])
 
-    # run predictions for all user history sizes and save timelog into file
-    with open('time.txt', 'a') as file:
-        for i in [5, 10, 15, 20, 24, 'all']:
-            start = perf_counter()
-            prediction(model, args, device, ckpt['category_dict'], ckpt['subcategory_dict'], n=i)
-            end = perf_counter()
-            file.write(f"For User History of size {i} took {timedelta(end-start)} minutes.\n")
+    prediction(model, args, device, ckpt['category_dict'], ckpt['subcategory_dict'])
 
 
-def reldiff(user, user_history, candidate_news):
-    rd = []
-    for n in candidate_news:
-        cn = n * user_history
-        l2 = np.linalg.norm(cn, axis=1)
-        l2 = np.stack([norm if norm != 0 else 1 for norm in l2])
-        rd.append(user - (cn.T / l2).T)
-    return np.mean(rd, axis=1)
-
-
-def prediction(model, args, device, category_dict, subcategory_dict, n):
+def prediction(model, args, device, category_dict, subcategory_dict):
     model.eval()
     with torch.no_grad():
         news_info, news_combined = get_news_feature(args, mode='test', category_dict=category_dict,
@@ -70,8 +48,7 @@ def prediction(model, args, device, category_dict, subcategory_dict, n):
         )
 
         f = open('prediction.txt', 'w', encoding='utf-8')
-
-        for cnt, (impids, log_vecs, log_mask, candidate_vec, user_news_history) in enumerate(dataloader.generate_batch()):  # ADDED
+        for cnt, (impids, log_vecs, log_mask, candidate_vec) in enumerate(dataloader.generate_batch()):
 
             if args.enable_gpu:
                 log_vecs = log_vecs.cuda(device=device, non_blocking=True)
@@ -80,34 +57,18 @@ def prediction(model, args, device, category_dict, subcategory_dict, n):
             user_vecs = model.user_encoder(
                 log_vecs, log_mask, user_log_mask=True).to(torch.device("cpu")).detach().numpy()
 
-            # include user_news_history
-            for id, user_vec, news_vec, hist_vec in zip(
-                    impids, user_vecs, candidate_vec, user_news_history):
-                
-                # replace original dot click predictor with RelDiff version
-                # score = np.dot(
-                #     news_vec, user_vec
-                # )
-                # pred_rank = (np.argsort(np.argsort(score)[::-1]) + 1).tolist()
-                
-                # obtain the reldiff size of user_news_history
-                if n != 'all':
-                    size = min(n, len(hist_vec))
-                else:
-                    size = len(hist_vec)
+            for id, user_vec, news_vec in zip(
+                    impids, user_vecs, candidate_vec):
 
-                score_reldiff = [np.dot(new, usr) for new, usr in zip(
-                   news_vec,
-                   reldiff(user_vec, hist_vec[:size], news_vec)
-                )]
-
-                pred_rank = (np.argsort(np.argsort(score_reldiff)[::-1]) + 1).tolist()
+                score = np.dot(
+                    news_vec, user_vec
+                )
+                pred_rank = (np.argsort(np.argsort(score)[::-1]) + 1).tolist()
                 f.write(str(id) + ' ' + '[' + ','.join([str(x) for x in pred_rank]) + ']' + '\n')
 
-         
         f.close()
 
-    zip_file = zipfile.ZipFile(f'ff-prediction-{n}.zip', 'w', zipfile.ZIP_DEFLATED)
+    zip_file = zipfile.ZipFile('prediction.zip', 'w')
     zip_file.write('prediction.txt')
     zip_file.close()
     os.remove('prediction.txt')
